3f3de0544714c02ac9f1005eb9c35bb7f498d5a0
Genepil1 <kublaijanarlaguda@gmail.com>
2025-04-26 02:18:56 +0800

    CUTSCENE(OPENING)/SKIP BUTTON

--- Assets/Scripts/System/MainMenu.cs
+++ Assets/Scripts/System/MainMenu.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
+    
 
-    public void PlayGame()
+    public void OnPlayButtonClicked()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("CUTSCENE");
+    
     }
+
 
     public void QuitGame()
     {
